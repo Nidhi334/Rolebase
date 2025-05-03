20e36c65bb8dc422496a6593657526e3b281b492
nidhi334 <knidhibharti8178@gmail.com>
2025-05-03 17:55:41 +0530

    Add Package model, serializer, and viewset; update URLs for package management

--- role/frontdesk/views.py
+++ role/frontdesk/views.py
@@ -88,6 +88,42 @@ class TestViewSet(viewsets.ModelViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     
+class PackageViewSet(viewsets.ModelViewSet):
+    queryset = Package.objects.all()
+    serializer_class = PackageSerializer
+    # permission_classes = [IsAuthenticated]
+
+    def post(self, request,*args, **kwargs):
+        data = request.data.copy()
+        serializers = self.get_serializer(data=data)
+        serializers.is_valid(raise_exception=True)
+        serializers.save(created_by=request.user)
+        return Response(serializers.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, *args, **kwargs):
+        user = request.user
+        instance = self.get_object()
+        data = request.data.copy()
+        if not user.is_staff and instance.created_by.id !=user.id:
+            return Response({"error":"You are not allowed to updated this objects."},
+                            status=status.HTTP_403_FORBIDDEN)
+        if 'created_by' in data:
+            del data['created_by']
+            serializers = self.get_serializer(instance, data=data, partial=True)
+            serializers.is_valid(raise_exception=True)
+            serializers.save()
+        return Response(serializers.data, status=status.HTTP_200_OK)
+    
+    def destroy(self, request, *args, **kwargs):
+        super().destroy(request, *args, **kwargs)
+        return Response(
+            {"message": "Package deleted successfully"},
+            status=status.HTTP_200_OK
+        )
+    
+    
+
+    
 
     
     
