56f56d456811131f7d4ebcd2fb10f5bf208fe337
rahulxdev0 <rk854327@gmail.com>
2025-05-03 15:36:16 +0530

    adding cors header

--- role/role/settings.py
+++ role/role/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'userapp',
     'frontdesk',
+    'corsheaders',
 ]
 AUTH_USER_MODEL = 'userapp.CustomUser'
 
@@ -60,6 +61,7 @@ SIMPLE_JWT = {
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -68,6 +70,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'role.urls'
 
