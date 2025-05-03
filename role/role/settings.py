d614fc8ece1b6ffea6eae2aaaba79b9076085d72
rahulxdev0 <rk854327@gmail.com>
2025-05-03 17:54:59 +0530

    minor changes

--- role/role/settings.py
+++ role/role/settings.py
@@ -70,12 +70,23 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 ROOT_URLCONF = 'role.urls'
 
 TEMPLATES = [
